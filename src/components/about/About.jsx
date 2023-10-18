182f2d9783222563a61ab2c4bc6fe29661bc4f38
Michijoe <e2296540@cmaisonneuve.qc.ca>
2023-10-18 11:52:34 -0400

    modif about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -29,7 +29,7 @@ const About = () => {
                             <small>AEC Conception et Développement de sites web</small>
                         </article>
                     </div>
-                    <p>Forte d’une expérience de plus de 5 ans en tant que technicienne à la mise en ligne de contenus audiovisuels pour diverses plateformes numériques web et télé, j’ai développé un intérêt croissant pour les aspects techniques du développement web.</p>
+                    <p>Forte d’une expérience de plus de 5 ans en tant que technicienne aux contenus audiovisuels pour diverses plateformes numériques web et télé, j’ai développé un intérêt croissant pour les aspects techniques du développement web.</p>
                     <p>J’ai donc décidé en 2022 d’acquérir les compétences nécessaires pour pouvoir évoluer professionnellement dans ce domaine qui me passionne. Je termine présentement ma formation en Conception et programmation de sites web et je souhaite mettre en pratique les compétences que j’ai acquises en tant que développeuse full-stack.</p>
                     <a href={CV} download className='btn'>Téléchargez mon CV</a>
                 </div>
